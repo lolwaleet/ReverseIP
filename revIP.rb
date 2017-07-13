205c2be9867238450af520e6f783a391ffd0c835
Abk Khan <iabkkhan@gmail.com>
2017-07-13 17:02:13 +0300

    Update revIP.rb
    >.<

--- revIP.rb
+++ revIP.rb
@@ -3,17 +3,18 @@ require 'colorize'
 require 'net/http'
 require 'mechanize'
 
-sep      = ('-'*86).colorize(:cyan)
+sep      = ('-'*100).colorize(:cyan)
 leets    = [ 'T3N38R15', 'MakMan', 'Maini', 'MithaiWala' ] 
 
-puts ' _____                              _____ _____  
-|  __ \                            |_   _|  __ \ 
-| |__) |_____   _____ _ __ ___  ___  | | | |__) |
-|  _  // _ \ \ / / _ \ \'__/ __|/ _ \ | | |  ___/ 
-| | \ \  __/\ V /  __/ |  \__ \  __/_| |_| |     
-|_|  \_\___| \_/ \___|_|  |___/\___|_____|_| ~ by '.colorize(:yellow) + 'AnonGuy'.colorize(:magenta)
+puts '
+                      _____                              _____ _____  
+                     |  __ \                            |_   _|  __ \ 
+                     | |__) |_____   _____ _ __ ___  ___  | | | |__) |
+                     |  _  // _ \ \ / / _ \ \'__/ __|/ _ \ | | |  ___/ 
+                     | | \ \  __/\ V /  __/ |  \__ \  __/_| |_| |     
+                     |_|  \_\___| \_/ \___|_|  |___/\___|_____|_| ~ by '.colorize(:yellow) + 'AnonGuy'.colorize(:magenta)
 
-print 'Greets to ~ '.colorize(:green)
+print '                     Greets to ~ '.colorize(:green)
 leets.each { |leet| (leet == leets.last ? (puts leet.colorize(:cyan)) : (print leet.colorize(:cyan) + ' -- ')) } # .. I know.
 puts sep
 
@@ -40,10 +41,8 @@ def getCMS(site) # lotta fps .-.
 	cms = ''
 	begin
 		cms  = Mechanize.new.get('http://' + site).at('meta[name="generator"]')[:content][0..50]
-		return cms.to_s.colorize(:yellow)
-	rescue StandardError
-		return '???'.colorize(:red)
-	end
+		return ' -- ' + '[ '.colorize(:cyan) + cms.to_s.colorize(:yellow) + ' ]'.colorize(:cyan)
+	rescue StandardError; end
 end
 
 if parsed['status'] == 'Fail'
@@ -56,11 +55,13 @@ else
 	domains = parsed['domainArray']
 	puts 'Domains -- '.colorize(:red)
 	domains.each.with_index(1) {|domain, index|
-		dashes     = '-' * (domains.flatten.max_by(&:size).length - domain[0].length + 1)
+		dashes     = '-' * (domains.flatten.max_by(&:size).length - domain[0].length + 1) + ' '
 		httpStatus = statusCode(domain[0])
-		cms        = getCMS(domain[0])
 		
-		puts '[ '.colorize(:cyan) + index.to_s.rjust(len, '0').colorize(:red) + ' ]'.colorize(:cyan) + ' -- ' + '[ '.colorize(:cyan) + domain[0].to_s.colorize(:green) + ' ] '.colorize(:cyan) + dashes.to_s + ' [ '.colorize(:cyan) + httpStatus.to_s + ' ]'.colorize(:cyan) + ' [ '.colorize(:cyan) + cms + ' ]'.colorize(:cyan)
+		print '[ '.colorize(:cyan) + index.to_s.rjust(len, '0').colorize(:red) + ' ]'.colorize(:cyan) + ' -- '
+		print '[ '.colorize(:cyan) + domain[0].to_s.colorize(:green) + ' ] '.colorize(:cyan) + dashes.to_s
+		print '[ '.colorize(:cyan) + httpStatus.to_s + ' ]'.colorize(:cyan)
+		puts getCMS(domain[0])
 	}
 end
 puts sep
